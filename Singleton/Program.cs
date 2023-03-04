3f78f4f2b5dfaf9c0f729b8649d2d9e90c6bb5f8
qmclouca <qmclouca@gmail.com>
2023-03-04 09:56:20 -0300

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Singleton
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton player1 = Singleton.GetInstance;
+            player1.Message("Jogador 1: A bola está comigo no meio de campo");
+
+            Singleton player2 = Singleton.GetInstance;
+            player2.Message("Jogador 2: Recebi a bola");
+
+            Singleton player3 = Singleton.GetInstance;
+            player3.Message("Jogador 3: Recebi a bola na linha de fundo");
+
+            Console.ReadKey();
+        }
+    }   
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null) instance = new Singleton();
+                return instance;
+            }
+        }
+        public void Message(string msg)
+        {
+            Console.Write(msg);
+        }           
+    }
+}

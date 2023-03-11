1073e5e29a52a0b8e6bde124079aa1714989e692
qmclouca <qmclouca@gmail.com>
2023-03-11 09:32:15 -0300

    Bridge

---
+++ Structural Patterns/Bridge/Culinaria.cs
@@ -0,0 +1,16 @@
+﻿namespace Bridge
+{
+    internal class Culinaria: ICanal
+    {
+        const string canal = "Canal de culinária.";
+        public string Canal()
+        {
+            return string.Format("Sintonizado no: {0}", canal);
+        }
+
+        public string Status()
+        {
+            return string.Format("Assistindo: {0}", canal);        
+        }
+    }
+}
---
+++ Structural Patterns/Bridge/SmartTv.cs
@@ -0,0 +1,22 @@
+﻿namespace Bridge
+{
+    public class SmartTv
+    {
+        //Aqui é onde o Bridge é aplicado
+        public ICanal? canalAtual { get; set; }
+        public void ExibeCanalSintonizado()
+        {
+            if (canalAtual != null)
+                Console.WriteLine(canalAtual.Canal());
+            else
+                Console.WriteLine("Por favor, selecione o canal.");
+        }
+        public void PlayTv()
+        {
+            if (canalAtual != null)
+                Console.WriteLine(canalAtual.Status());
+            else
+                Console.WriteLine("Por favor, selecione um canal.");
+        }
+    }
+}

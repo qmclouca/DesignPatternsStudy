d105fe75ec64919f76e9005473ac4c5b5bed3ee0
qmclouca <qmclouca@gmail.com>
2023-03-13 18:29:58 -0300

    Composite

---
+++ Structural Patterns/Composite/Composite.cs
@@ -0,0 +1,31 @@
+﻿namespace Composite
+{
+    internal class Composite: Component
+    {
+        private List<Component> children = new List<Component>();
+
+        public Composite(string name): base(name)
+        {
+        }
+
+        public override void Add(Component component)
+        {
+            children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+
+            foreach (Component component in children)
+            {
+                component.Display(depth + 2);
+            }
+        }   
+    }
+}
---
+++ Structural Patterns/Composite/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Composite root = new Composite("ROOT");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
+
+            Composite comp = new Composite("Composite X");
+            comp.Add(new Leaf("Leaf XA"));
+            comp.Add(new Leaf("Leaf XB"));
+
+            root.Add(comp);
+            root.Add(new Leaf("Leaf C"));
+
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+            root.Remove(leaf);
+            root.Display(1);
+
+            Console.ReadKey();
+        }
+    }  
+}

8cbd27761729e74b9f31b9cfed6ee9c6eb48802c
qmclouca <qmclouca@gmail.com>
2023-03-28 08:55:12 -0300

    Cadeia de responsabilidade

---
+++ Behavior Patterns/ChainOfResponsibility/ConcreteHandler1.cs
@@ -0,0 +1,17 @@
+﻿namespace ChainOfResponsibility
+{
+    public class ConcreteHandler1 : Handler
+    {
+        public override void HandlerRequest(int request)
+        {
+            if (request >= 0 && request <= 10)
+            {
+                Console.WriteLine("{0} handled request {1}", this.GetType().Name, request);
+            }
+            else if (sucessor != null)
+            {
+                sucessor.HandlerRequest(request);
+            }
+        }
+    }
+}
---
+++ Behavior Patterns/ChainOfResponsibility/Program.cs
@@ -0,0 +1,27 @@
+﻿using ChainOfResponsibility;
+
+namespace ChainResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup Chain of Responsibility
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+            h1.SetSucessor(h2);
+            h2.SetSucessor(h1);
+
+            // Generate and process request
+            int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
+
+            foreach (int request in requests)
+            {
+                h1.HandlerRequest(request);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}

cc3c77ec9d47e9f61af9b2176a0eb0dfc359dd74
qmclouca <qmclouca@gmail.com>
2023-04-02 09:26:20 -0300

    Iterator

---
+++ Behavior Patterns/Interator/ConcreteIterator.cs
@@ -0,0 +1,36 @@
+﻿namespace Iterator
+{
+    public class ConcreteIterator : Iterator
+    {
+        private ConcreteAggregate _aggregate;
+        private int _current = 0;
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            _aggregate = aggregate;
+        }
+        public override object CurrentItem()
+        {
+            return _aggregate[_current];
+        }
+
+        public override object First()
+        {
+            return _aggregate[0];
+        }
+
+        public override bool IsDone()
+        {
+            return _current >= _aggregate.Count;
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            if (_current < _aggregate.Count - 1)
+            {
+                ret = _aggregate[++_current];
+            }
+            return ret;
+        }
+    }
+}

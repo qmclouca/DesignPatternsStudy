a5dccf768fec4d732b33ec0a3962582d5ce8cf2f
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-03 20:40:08 -0300

    Mediator

---
+++ Behavior Patterns/Mediator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator m = new ConcreteMediator();
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+
+            c1.Send("Como você está?");
+            c2.Send("Bem, obrigado!");
+
+            Console.ReadKey();
+        }
+    }
+}

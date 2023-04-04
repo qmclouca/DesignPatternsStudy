a4e7d509b6b85bd000beeea4e0c250da8deca076
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-04 18:30:04 -0300

    Memento

---
+++ Behavior Patterns/Memento/Memento.cs
@@ -0,0 +1,15 @@
+﻿namespace Memento
+{
+    public class Memento
+    {
+        private string _state;
+        public Memento(string state)
+        {
+            _state = state;
+        }
+        public string State
+        {
+            get { return _state; }            
+        }
+    }
+}
---
+++ Behavior Patterns/Memento/Originator.cs
@@ -0,0 +1,25 @@
+﻿namespace Memento
+{
+    public class Originator
+    {
+        private string _state;
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine("State = " + _state);
+            }
+        }
+        public Memento CreateMemento()
+        {
+            return (new Memento(_state));
+        }
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memento.State;
+        }
+    }
+}

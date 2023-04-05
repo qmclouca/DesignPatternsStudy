babb856e2ad6622fe90cb61b64a5edf642702d0c
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-05 17:58:24 -0300

    Observer

---
+++ Behavior Patterns/Observer/Observer.cs
@@ -0,0 +1,8 @@
+﻿namespace Observer
+{
+    public abstract class Observer
+    {      
+        public abstract void Update();
+        
+    }
+}
---
+++ Behavior Patterns/Observer/Subject.cs
@@ -0,0 +1,22 @@
+﻿namespace Observer
+{
+    public abstract class Subject
+    {
+        private List<Observer> observers = new List<Observer>();
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+        public void Notify()
+        {
+            foreach (Observer observer in observers)
+            {
+                observer.Update();
+            }
+        }       
+    }
+}

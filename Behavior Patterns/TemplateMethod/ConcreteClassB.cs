d573105724311d9312eedb230c401a4808996aa1
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-08 20:03:13 -0300

    Template Method

---
+++ Behavior Patterns/TemplateMethod/ConcreteClassB.cs
@@ -0,0 +1,14 @@
+﻿namespace TemplateMethod
+{
+    public class ConcreteClassB: AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+        }
+    }
+}
